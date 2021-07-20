e4429e3c120a2b27949ae19a3a6b75ef8bf2fac3
Heathrxw <Heathrxw@gmail.com>
2021-07-20 21:04:55 +0100

    Added overworld coordinate calculator

---
+++ SMP/Coordinates.py
@@ -0,0 +1,7 @@
+GetX = int(input("Enter the X coordinates of the overworld "))
+GetZ = int(input("Enter the Z coordinates of the overworld "))
+
+NetherCoordinatesX = GetX/8
+NetherCoordinatesZ = GetZ/8
+
+print("The Nether Coordinates are:", "X: " +str(NetherCoordinatesX), "Z: " + str(NetherCoordinatesZ))
---
+++ Skyblock/MinionCalculator.py
@@ -0,0 +1,9 @@
+print("Welcome to the Minion Calculator for Hypixel Skyblock")
+print("Enter the values that are related to the variable!")
+
+FuelSpeed = input("Enter your Fuel Speed: ")
+MinionType = input("Enter your type of Minion: ")
+MinionLevel = input("Enter the level of your Minion: ")
+MinionCount = input("Enter the amount of this type of Minion: ")
+
+print("Your Items per hour are: "(MinionCount*Minion))
